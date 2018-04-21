fa4b33260dcc36edd4f36205472aabc059e0123c
raybloo <boris.zbinden@epfl.ch>
2018-04-21 23:57:42 +0200

    On est pas fatiguégit add *

---
+++ Assets/Scripts/GroundCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollider : MonoBehaviour {
+    CatMovement parentscript;
+
+    // Use this for initialization
+    void Start () {
+        parentscript = GetComponentInParent<CatMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.tag != "Cat") {
+            parentscript.grounded++;
+            Debug.Log("grounded!");
+        } 
+    }
+
+    void OnTriggerExit2D(Collider2D coll) {
+        if (coll.gameObject.tag != "Cat") {
+            parentscript.grounded--;
+            Debug.Log("décollage!");
+        }
+    }
+}
