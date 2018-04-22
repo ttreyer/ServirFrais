6c600fde3c96966a526a1d75631df86fcb64bbba
KL <kim.phanhoang@epfl.ch>
2018-04-22 21:53:04 +0200

    Maybe change catmovement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
     public float speed = 10.0f;
     public Transform[] targets;
-	
-	// Update is called once per frame
-	void Update () {
-        float centerX = 0;
-        foreach (Transform target in targets)
-            centerX += target.position.x;
-        centerX /= targets.Length;
+    private CatMovement cat;
+    private FishMovement fish;
 
-        Vector3 center = new Vector3(centerX, transform.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, center, speed * Time.deltaTime);
-	}
+    private void Start()
+    {
+
+        cat = GameObject.FindGameObjectsWithTag("Cat")[0].GetComponent<CatMovement>();
+        fish = GameObject.FindGameObjectsWithTag("Fish")[0].GetComponent<FishMovement>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (cat.isAlive())
+        {
+            float centerX = 0;
+            foreach (Transform target in targets)
+                centerX += target.position.x;
+            centerX /= targets.Length;
+
+            Vector3 center = new Vector3(centerX, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, center, speed * Time.deltaTime);
+        }
+    }
 }
---
+++ Assets/textEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class textEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text theText;
+
+    public void Start()
+    {
+        theText = GetComponent<Text>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = Color.red; //Or however you do your color
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = Color.white; //Or however you do your color
+    }
+}
