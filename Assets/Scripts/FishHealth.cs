428f107e321a4b0457e6912cdfccdcb21a20e969
Thierry Treyer <zehkae@gmail.com>
2018-04-22 15:13:18 +0200

    Fish can die

---
+++ Assets/Scripts/FishHealth.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishHealth : MonoBehaviour {
+    public int maxHealth = 3;
+    public float invulnDuration = 0.5f;
+    public float knockbackRatio = 3.0f;
+
+    private int health;
+    private float invulnTimer = 0.0f;
+
+    private Rigidbody2D rbody;
+    private FishMovement movement;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        health = maxHealth;
+        rbody = GetComponent<Rigidbody2D>();
+        movement = GetComponent<FishMovement>();
+        animator = GetComponent<Animator>();
+	}
+
+    void Update()
+    {
+        invulnTimer -= Time.deltaTime;
+        if (invulnTimer < 0.0f) {
+            movement.enabled = true;
+            animator.SetBool("isKnocked", false);
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (invulnTimer > 0.0f)
+            return;
+
+        // Start invulnerability, but can't move
+        invulnTimer = invulnDuration;
+        movement.enabled = false;
+        animator.SetBool("isKnocked", true);
+        rbody.velocity = - knockbackRatio * rbody.velocity;
+
+        health -= amount;
+        if (health <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        invulnTimer = Mathf.Infinity;
+        movement.enabled = false;
+        animator.SetBool("isKnocked", false);
+        animator.SetTrigger("die");
+    }
+}
---
+++ Assets/Scripts/FishLineDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishLineDamage : MonoBehaviour {
+    public int damageAmount = 1;
+
+    private FishHealth fish;
+
+	// Use this for initialization
+	void Start () {
+        fish = GameObject.FindGameObjectWithTag("Fish").GetComponent<FishHealth>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Fish"))
+            fish.TakeDamage(damageAmount);
+    }
+}
